d8c53ac70c612da5d03e38cd0da98dc6b5fee931
batman8056 <manimanish8056@gmail.com>
2024-11-06 17:57:32 +0530

    update

---
+++ whoIspaying.js
@@ -0,0 +1,13 @@
+const whosPaying = (names) => {
+    
+    /******Don't change the code above*******/
+        
+        //Write your code here.
+        const randomIndex = Math.floor(Math.random() * names.length);
+        const selectedPerson = names[randomIndex];
+        return `${selectedPerson} is going to buy lunch today!`;
+         
+    /******Don't change the code below*******/    
+    }
+
+console.log(whosPaying(["Angela", "Ben", "Jenny", "Michael", "Chloe"]))
