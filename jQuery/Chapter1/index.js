84ca891d384b07a605b1cf733a8036b68a25345a
batman8056 <manimanish8056@gmail.com>
2024-11-21 21:09:32 +0530

    update

---
+++ jQuery/Chapter1/index.js
@@ -0,0 +1,24 @@
+// If we give Script inside header section we need to define like this
+// $(document).ready(function(){
+//     $("h1").css("color", "red");
+// });
+
+
+
+// //other wise we simply use like this
+// console.log($("h1").css("color"));//we will get color value in h1
+
+// console.log($("button").css("color", "red").css("color"));//select all the button
+
+
+// $("h1").addClass("big-title");//we assign new class in h1 here based on class css can apply
+
+
+// $("h1").removeClass("big-title");//here we removed class in h1
+
+
+$("h1").addClass("big-title margin-50");//we can add multiple class in single method
+
+
+
+
